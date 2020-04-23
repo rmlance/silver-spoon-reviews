5b794935b0388f1f2545eb729e13e4fe4d8981c2
rmlance <reidlance.rml@gmail.com>
2020-04-23 16:46:32 -0400

    Fixed indendation

--- app/javascript/react/components/RestaurantsIndexComponent.js
+++ app/javascript/react/components/RestaurantsIndexComponent.js
@@ -3,40 +3,40 @@ import React, { useState, useEffect } from 'react'
 import RestaurantTile from './RestaurantTile'
 
 const RestaurantsIndexComponent = props => {
-    const [restaurants, setRestaurants] = useState([])
+  const [restaurants, setRestaurants] = useState([])
 
-    useEffect(()=> {
+  useEffect(()=> {
     fetch('/api/v1/restaurants', {
-        credentials: "same-origin"
+      credentials: "same-origin"
     })
-        .then(response => {
-        if(response.ok) {
-            return response
-        } else {
-            let errorMessage = `${response.status} (${response.statusText})`
-                error = new Error(errorMessage)
-            throw(error)
-        }
+    .then(response => {
+      if(response.ok) {
+        return response
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`
+        error = new Error(errorMessage)
+        throw(error)
+      }
     })
     .then(response => response.json())
     .then(parsedRestaurantData => setRestaurants(parsedRestaurantData))
     .catch(error => console.error(`Error in fetch: ${errorMessage}`))
-    }, [])
+  }, [])
 
-    const restaurantsList = restaurants.map(restaurant => {
-        return (
-            <RestaurantTile
-            key={restaurant.id}
-            restaurant={restaurant}
-            />
-        )
-    })
-
+  const restaurantsList = restaurants.map(restaurant => {
     return (
-    <div>
-        {restaurantsList}
-    </div>
+      <RestaurantTile
+      key={restaurant.id}
+      restaurant={restaurant}
+      />
     )
+  })
+
+  return (
+    <div>
+    {restaurantsList}
+    </div>
+  )
 }
 
 export default RestaurantsIndexComponent
