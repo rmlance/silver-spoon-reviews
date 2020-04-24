f798cbf5926201084012054e6d1adb82e05b9bd4
rmlance <reidlance.rml@gmail.com>
2020-04-24 16:45:17 -0400

    Started integrating foundation, added basic style for Index page only.

--- app/javascript/react/components/RestaurantTile.js
+++ app/javascript/react/components/RestaurantTile.js
@@ -3,7 +3,11 @@ import React from 'react'
 const RestaurantTile = props => {
 
   return (
-    <div>{props.restaurant.name}</div>
+    <div  className="cell callout text-center medium-4">
+      <h3>
+        {props.restaurant.name}
+        </h3>
+    </div>
   )
 }
 
