e32811612f89c260b2dc7ea9cd6382b097a6d222
rmlance <reidlance.rml@gmail.com>
2020-04-29 13:07:43 -0400

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :restaurants, only: [:index, :show, :create] do
-        resources :reviews
-      end
+      resources :restaurants, only: [:index, :show, :create]
     end
   end
 
